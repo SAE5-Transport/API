4a25a39b456f46ba82612b3fff3298ac76f164c9
Jouca <progameur009@gmail.com>
2025-02-11 10:24:37 +0100

    update(dev): Update order for stations on findLocation

--- src/api/services/otp.py
+++ src/api/services/otp.py
@@ -19,6 +19,35 @@ def getStations(name):
         if 'data' in response.json():
             data = response.json()['data']['stations']
 
+            modes = []
+
+            linesDataSet = {}
+            for station in data:
+                for stop in station["stops"]:
+                    for route in stop["routes"]:
+                        if route["gtfsId"] not in linesDataSet:
+                            linesDataSet[route["gtfsId"]] = route
+
+            for line in linesDataSet.values():
+                modes.append(line["mode"])
+
+            # Order the stations
+            orders = {
+                'SUBWAY': 0,
+                'RAIL': 1,
+                'TRAM': 2,
+                'FERRY': 3,
+                'CABLE_CAR': 4,
+                'BUS': 5,
+            }
+
+            # Get highest mode for each station
+            for station in data:
+                station["mode"] = min([orders[line["mode"]] for stop in station["stops"] for line in stop["routes"]])
+
+            # Sort stations by mode
+            data.sort(key=lambda x: x["mode"])
+
             return data
         
     return {"error": "No data found"}
