d8acf436ed18e3cfb137676d62059c2b0ca38878
L3ChatNoir <108089371+L3ChatNoir@users.noreply.github.com>
2025-02-23 18:34:08 +0100

    docs(health): Swagger update

--- src/api/api/v1/__init__.py
+++ src/api/api/v1/__init__.py
@@ -1,18 +1,26 @@
 '''
     ENDPOINTS
 '''
+from apifairy import response, other_responses, arguments
 from flask import Blueprint
+from flask_marshmallow import Marshmallow
 from .search import search_bp
 from ...services.postgres import health as pghealth
 from ...services.mongo import health as mongohealth
 
 v1_bp = Blueprint("v1", __name__, url_prefix='/v1')
 v1_bp.register_blueprint(search_bp)
+ma = Marshmallow(search_bp)
 
+class HealthResponse(ma.Schema):
+    postegresql = ma.Boolean(description="Connection to postgresql is ok?")
+    mongodb = ma.Boolean(description="Connection to mongodb is ok?")
+    
 @v1_bp.route('/health', strict_slashes=False, methods=['GET'])
+@response(HealthResponse)
 def healthcheck():
     '''
-        Check if all services health
+        Check if all services are connected
     '''
     main_data = {
         "postegresql": pghealth(),
